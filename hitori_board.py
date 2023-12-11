b21f98bebf4bf9b2781b52c6509f39a1c1ba6b69
VinkiLilai <anastasia.gorodnichaya@gmail.com>
2023-12-11 19:37:45 +0500

    Add base board

--- hitori_board.py
+++ hitori_board.py
@@ -1,26 +1,40 @@
-from enum import Enum
-from hitori_commands import Command, CommandManager
-
-
-class SetBlack(Command):
-    def __init__(self) -> None:
-        super().__init__()
-
-
-class CellState(Enum):
-    UNKNOWN = 0
-    WHITE = 1
-    BLACK = 2
-
-
-class HitoriCell:
-    def __init__(self, x, y, value) -> None:
-        self.x = x
-        self.y = y
-        self.value = value
-        self.state = CellState.UNKNOWN
+from hitory_cell import CellState, HitoriCell
+import numpy as np
 
 
 class HitoriBoard:
     def __init__(self, width, height, numbers) -> None:
+        if len(numbers) != width * height:
+            raise
+
+        self._cells = []
+        self.width = width
+        self.height = height
+
+        i = 0
+        for y in range(height):
+            for x in range(width):
+                if i < len(numbers):
+                    self.cells.append(HitoriCell(x, y, numbers[i]))
+                    i += 1
+
+        self._board = np.array(self.cells).reshape(height, width)
+
+    def get_row(self, number):
+        return self._board[number]
+
+    def get_col(self, number):
+        return np.transpose(self._board)[number]
+
+    def get_adjacent(self, x, y):
+        dx = [-1, 1]
+        dy = [-1, 1]
+        adjacent = []
+        for deltaX in dx:
+            for deltaY in dy:
+                if 0 < x + deltaX < self.width and 0 < y + deltaY < self.height:
+                    adjacent.append(self._board[x + deltaX, y + deltaY])
+        return adjacent
+
+    def сheck_сonnectivity(self):
         pass
