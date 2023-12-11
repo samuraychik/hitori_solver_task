f67a6f80c42ccb4170b6992d7a5e012b5f7269eb
samuraychik <vitvit0312@gmail.com>
2023-12-11 16:29:03 +0500

    started stuff

---
+++ hitori_board.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class CellState(Enum):
+    UNKNOWN = 0
+    WHITE = 1
+    BLACK = 2
+
+
+class HitoriCell:
+    def __init__(self, x, y, value) -> None:
+        self.x = x
+        self.y = y
+        self.value = value
+        self.state = CellState.UNKNOWN
+
+
+class HitoriBoard:
+    def __init__(self, width, height, numbers) -> None:
+        pass
---
+++ hitori_commands.py
@@ -0,0 +1,36 @@
+class Command:
+    def do(self):
+        raise NotImplementedError()
+    
+    def undo(self):
+        raise NotImplementedError()
+    
+    def name(self):
+        raise NotImplementedError()
+    
+
+class CommandManager:
+    def __init__(self) -> None:
+        self.history = list()
+        self.trash = list()
+    
+    def do(self, command: Command) -> None:
+        command.do()
+        self.history.append(command)
+        self.trash = list()
+        
+    def undo(self) -> None:
+        try:
+            command = self.history.pop()
+            command.undo()
+            self.trash.append(command)
+        except IndexError:
+            print("<ERROR> History is empty")
+    
+    def redo(self) -> None:
+        try:
+            command = self.trash.pop()
+            command.do()
+            self.history.append(command)
+        except IndexError:
+            print("<ERROR> Trash is empty")
